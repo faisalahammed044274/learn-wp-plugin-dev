3707c7968d7c190509b2e66e8a7f2041cc57213a
faisalahammed044274 <faisalahammed044274@gmail.com>
2025-01-27 14:01:53 +0600

    first commit

---
+++ wp-content/plugins/faisal-plugin/faisal-plugin.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * @package Faisalplugin
+ */
+/**
+ * Plugin Name: Faisal Plugin
+ * Plugin URI: http://github.com/faisalahammed044274/faisal-plugin
+ * Description: This is my first attempt on writing a custom Plugin.
+ * Version: 1.0.0
+ * Author: Faisal
+ * Author URI: http://github.com/faisalahammed044274
+ * License: GPLv2 or later
+ * Text Domain: faisal-plugin
+ */
+
+/*
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License, version 2, as published by the Free Software Foundation. You may NOT assume that you can use any other version of the GPL.
+We also offer this code under the MIT license. You may use it however you wish.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+// if ( ! defined( 'ABSPATH' ) ) {
+//     die;
+// }   // this is a security feature to prevent direct access to the plugin file.
+
+
+// if ( ! function_exists( 'add_action' ) ) {
+    //     echo 'Hey, you can\t access this file, you silly human!';
+    //     exit;
+    // }
+    
+defined( 'ABSPATH' ) or die( 'Hey, you can\t access this file, you silly human!' );
+
+
+class FaisalPlugin 
+{
+    //methods
+}
+
+$faisalPlugin = new FaisalPlugin();
+
+
