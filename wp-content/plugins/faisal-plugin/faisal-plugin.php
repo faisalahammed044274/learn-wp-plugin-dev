67d66e3395f8b6082174cfd895af0bcaf7840f92
faisalahammed044274 <faisalahammed044274@gmail.com>
2025-01-27 15:33:15 +0600

    Custom Post Type

--- wp-content/plugins/faisal-plugin/faisal-plugin.php
+++ wp-content/plugins/faisal-plugin/faisal-plugin.php
@@ -29,15 +29,47 @@ This program is distributed in the hope that it will be useful, but WITHOUT ANY 
     //     echo 'Hey, you can\t access this file, you silly human!';
     //     exit;
     // }
-    
-defined( 'ABSPATH' ) or die( 'Hey, you can\t access this file, you silly human!' );
 
+
+defined( 'ABSPATH' ) or die( 'Hey, you can\'t access this file, you silly human!' );
 
 class FaisalPlugin 
 {
     //methods
+    function __construct() {
+        add_action( 'init', array( $this, 'custom_post_type' ) );
+    }
+        function activate() {
+            // generated a CPT
+            $this->custom_post_type();
+            // flush rewrite rules
+            flush_rewrite_rules();
+        }
+
+        function deactivate() {
+            // flush rewrite rules
+            flush_rewrite_rules();
+        }
+
+        function uninstall() {
+            // delete CPT
+            // delete all the plugin data from the DB
+        }
+
+        function custom_post_type(){
+            // register a custom post type
+            // register a custom taxonomy
+            register_post_type( 'book', ['public' => true, 'label' => 'Books'] );
+        }
 }
 
-$faisalPlugin = new FaisalPlugin();
+if (class_exists('FaisalPlugin')) {
+    $faisalPlugin = new FaisalPlugin();
+}
 
-
+// activation
+register_activation_hook( __FILE__, array( $faisalPlugin, 'activate' ) );
+// deactivation
+register_deactivation_hook( __FILE__, array( $faisalPlugin, 'deactivate' ) );
+// uninstall
+// register_uninstall_hook( __FILE__, array( $faisalPlugin, 'uninstall' ) );
