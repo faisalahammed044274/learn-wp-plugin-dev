6a1c42bd14d9d4bd2ddf78dde0be6ae24c46bb8f
faisalahammed044274 <faisalahammed044274@gmail.com>
2025-01-29 18:01:04 +0600

    Composer Autoload added

---
+++ wp-content/plugins/faisal-plugin/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'd2s/faisal-plugin',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '29b5a5eb075475a9061ddb1eefc8f769463dd1f8',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'd2s/faisal-plugin' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '29b5a5eb075475a9061ddb1eefc8f769463dd1f8',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
